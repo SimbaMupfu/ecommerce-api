2fd1cb0dc49b9c32b47ba1383a185af01bc62123
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-25 01:22:25 +0200

    Create the Product service and make it fetch all products.

---
+++ src/main/java/inc/sims/hustles/service/ProductService.java
@@ -0,0 +1,19 @@
+package inc.sims.hustles.service;
+
+import inc.sims.hustles.model.Product;
+import inc.sims.hustles.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+}
