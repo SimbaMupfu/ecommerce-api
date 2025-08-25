ff17166eb362a56729d27cb1c552ff558ce8e708
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-25 02:12:59 +0200

    Allow user to fetch one product be ID.

--- src/main/java/inc/sims/hustles/service/ProductService.java
+++ src/main/java/inc/sims/hustles/service/ProductService.java
@@ -18,6 +18,6 @@ public class ProductService {
     }
 
     public Product getProductById(int productId){
-        return productRepository.findById(productId).orElse(new Product());
+        return productRepository.findById(productId).orElse(null);
     }
 }
